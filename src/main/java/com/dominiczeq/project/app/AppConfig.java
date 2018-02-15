d7429512588c424dd2798ee2f0e0914371f437fd
Dawid Tyrała <dominiczeq@github.com>
2018-02-15 12:57:43 +0100

    Corrected configuration in AppConfig and persistence.xml files

--- src/main/java/com/dominiczeq/project/app/AppConfig.java
+++ src/main/java/com/dominiczeq/project/app/AppConfig.java
@@ -8,12 +8,15 @@ import javax.validation.Validator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleContextResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -23,6 +26,9 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 @ComponentScan(basePackages = { "com.dominiczeq" })
 
+@EnableWebMvc
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages= {"com.dominiczeq.repository"})
 public class AppConfig extends WebMvcConfigurerAdapter {
 
 	@Bean
