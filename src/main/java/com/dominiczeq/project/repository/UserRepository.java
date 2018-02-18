3685baff9efda0bd764badc98b320720e4329d37
Dawid Tyrała <dominiczeq@github.com>
2018-02-18 14:04:16 +0100

    Added update borrowedBooksfor User

--- src/main/java/com/dominiczeq/project/repository/UserRepository.java
+++ src/main/java/com/dominiczeq/project/repository/UserRepository.java
@@ -1,9 +1,15 @@
 package com.dominiczeq.project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.dominiczeq.project.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+	@Modifying
+	@Query(nativeQuery = true, value = "UPDATE user SET borrowedBooks=?1 WHERE id=?2")
+	void setBorrowedBook(Integer borrowedBooks, Long userId);
 
 }
