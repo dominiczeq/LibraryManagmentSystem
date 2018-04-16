77fce76ebbc3bc7739b9c6543e13d86d3927cc2b
Dawid Tyrała <dominiczeq@github.com>
2018-04-16 13:17:59 +0200

    Changed database account

--- src/main/java/com/dominiczeq/project/app/AppConfig.java
+++ src/main/java/com/dominiczeq/project/app/AppConfig.java
@@ -41,7 +41,7 @@ public class AppConfig extends WebMvcConfigurerAdapter {
 	@Bean
 	public LocalEntityManagerFactoryBean entityManagerFactory() {
 		LocalEntityManagerFactoryBean emfb = new LocalEntityManagerFactoryBean();
-		emfb.setPersistenceUnitName("sql11224141");
+		emfb.setPersistenceUnitName("sql7232972");
 		return emfb;
 	}
 
